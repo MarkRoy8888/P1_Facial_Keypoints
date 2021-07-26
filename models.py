e47a2523e20f1bd0168245c8e498c98fc6c94fcd
Mark <markroy8888@gmail.com>
2021-07-26 21:53:55 +0800

    update

--- models.py
+++ models.py
@@ -23,23 +23,25 @@ class Net(nn.Module):
         self.conv1 = nn.Sequential(nn.Conv2d(1, 32, 3), # 32*222*222
                                    nn.ReLU(),
                                    nn.MaxPool2d(2),
-                                   nn.Dropout(0.1)) # 1*111*111
+                                   nn.Dropout(0.25)) # 1*111*111
         self.conv2 = nn.Sequential(nn.Conv2d(32, 64, 3), # 64*111*111
                                    nn.ReLU(),
                                    nn.MaxPool2d(2,ceil_mode=True),) # 64*56*56
         self.conv3 = nn.Sequential(nn.Conv2d(64, 128, 3), # 128*54*54
                                    nn.ReLU(),
                                    nn.MaxPool2d(2,ceil_mode=True),
-                                   nn.Dropout(0.1)) # 128*27*27
+                                   nn.Dropout(0.25)) # 128*27*27
         self.conv4 = nn.Sequential(nn.Conv2d(128, 256, 3), # 256*54*54
                                    nn.ReLU(),
                                    nn.MaxPool2d(2,ceil_mode=True),
-                                   nn.Dropout(0.1))
+                                   nn.Dropout(0.3))
         
         
         
         
-        self.fc1 = nn.Linear(258*19*19, 2*68)
+        self.fc1 = nn.Linear(93312, 4*68)
+        self.fc1_drop = nn.Dropout(p=0.4)
+        self.fc2 = nn.Linear(4*68, 2*68)
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
@@ -54,5 +56,7 @@ class Net(nn.Module):
         x = self.conv3(x)
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
         # a modified x, having gone through all the layers of your model, should be returned
         return x
